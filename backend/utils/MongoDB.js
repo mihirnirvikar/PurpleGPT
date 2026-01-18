49d634fe101b2b868ebff392f9612a1702e2fdb9
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2026-01-18 17:06:16 +0530

    feat: add mongodb atlas url

--- backend/utils/MongoDB.js
+++ backend/utils/MongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connectMongoDB() {
   try{
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_ATLAS_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
