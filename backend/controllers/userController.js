6d7603b5937ae01b48b3a3414fe779e8d63140fb
Mihir Nirvikar <mihirnirvikar3@gmail.com>
2026-02-16 15:33:16 +0530

    feat: add contact mail route for portfolio via purplegpt backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,70 @@
 const UserModel = require("../models/authModel");
+const transporter = require("../utils/nodemailer");
 
-const getUserInfo = async(req, res) => {
-    try {
-        const {userId} = req.user;
-        
-        const user = await UserModel.findOne({_id: userId});
+const getUserInfo = async (req, res) => {
+  try {
+    const { userId } = req.user;
 
-        if(!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
+    const user = await UserModel.findOne({ _id: userId });
 
-        const userDetails = {
-            name: user.name,
-            email: user.email,
-            isAccountVerified: user.isAccountVerified,
-        }
-
-        return res.status(200).json({message: "User details fetched successfully", data: userDetails});
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ error: "Internal Server Error" });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-}
 
+    const userDetails = {
+      name: user.name,
+      email: user.email,
+      isAccountVerified: user.isAccountVerified,
+    };
 
+    return res.status(200).json({
+      message: "User details fetched successfully",
+      data: userDetails,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const sendMessage = async (req, res) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    // send mail to user
+    const mailSender = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Message sent successfully",
+      text:
+        `Hi ${name},\n\n\nYour message has been received. We will get back to you soon,\n` +
+        `Best,\nMihir Nirvikar`,
+    };
+
+    transporter.sendMail(mailSender).catch(console.error);
+
+    // send mail to admin
+    const mailReceiver = {
+      from: process.env.SENDER_EMAIL,
+      to: process.env.RECEIVER_EMAIL,
+      subject: "Message receive from " + name,
+      text: `Hi, I'm ${name} (${email})\n\n\n${message}\n\nBest,\n${name}`,
+    };
+
+    transporter.sendMail(mailReceiver).catch(console.error);
+
+    return res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 module.exports = {
-    getUserInfo
-}
+  getUserInfo,
+  sendMessage,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const userAuth = require("../middleware/userAuth");
 
-const { getUserInfo } = require("../controllers/userController");
+const { getUserInfo, sendMessage } = require("../controllers/userController");
 
 userRouter.get("/get-user-info", userAuth, getUserInfo);
-
+userRouter.post("/send-message", sendMessage);
 
 module.exports = userRouter;
